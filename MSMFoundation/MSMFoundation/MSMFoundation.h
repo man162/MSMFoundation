7e38a0d4c79ce9f07238a7532f9be305b1d749a5
Manpreet Singh <manpreet.singh@cineplex.com>
2020-10-19 18:29:25 -0400

    adding initial project

---
+++ MSMFoundation/MSMFoundation/MSMFoundation.h
@@ -0,0 +1,18 @@
+//
+//  MSMFoundation.h
+//  MSMFoundation
+//
+//  Created by Manpreet Singh on 2020-10-19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MSMFoundation.
+FOUNDATION_EXPORT double MSMFoundationVersionNumber;
+
+//! Project version string for MSMFoundation.
+FOUNDATION_EXPORT const unsigned char MSMFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MSMFoundation/PublicHeader.h>
+
+
